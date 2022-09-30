6a1b55d7a3c7ed059036466d7766347acdabd4ed
Solution4904 <shinjseoup@gmail.com>
2022-09-30 16:28:24 +0900

    no message

---
+++ app/src/main/java/com/solution/algorithm/정수 제곱근 판별.kt
@@ -0,0 +1,17 @@
+package com.solution.algorithm
+
+/*
+코딩테스트 연습 > 연습문제 > 자릿수 더하기
+https://school.programmers.co.kr/learn/courses/30/lessons/12934
+*/
+
+fun main() {
+    fun solution(n: Long): Long {
+        for (i in 0..n)
+            if (i * i == n) return (i + 1) * (i + 1)
+        return -1
+    }
+
+    val result = solution(3)
+    print(result)
+}
